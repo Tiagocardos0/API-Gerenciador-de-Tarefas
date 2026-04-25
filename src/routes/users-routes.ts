2e70f55532472a2372474aa83dbe1854449343aa
Tiago-Cardoso <tiago.card2121@gmail.com>
2026-04-25 20:00:00 -0300

    criado middleware de autorização de usuario e ajustado a rota

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { UsersController } from "@/controllers/Users-controller";
-import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 const usersController = new UsersController();
 
-usersRoutes.post("/", ensureAuthenticated, usersController.create);
+usersRoutes.post("/", usersController.create);
 
 export { usersRoutes };
