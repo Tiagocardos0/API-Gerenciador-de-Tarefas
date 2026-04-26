4d457ef4370b68c0f2d1f19d8cab533105664701
Tiago-Cardoso <tiago.card2121@gmail.com>
2026-04-26 15:56:33 -0300

    ajustado rota de tasks

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 
 import { TaskController } from "@/controllers/Task-controller";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
-import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+
 
 const taskController = new TaskController();
 
 const taskRoutes = Router();
-taskRoutes.use(ensureAuthenticated, verifyUserAuthorization(["ADMIN", "MEMBER"]));
+taskRoutes.use(ensureAuthenticated);
 
 taskRoutes.post('/', taskController.create);
 taskRoutes.get('/', taskController.index);
